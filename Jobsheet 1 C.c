da40d7d794b3ece9b4154527d5ead0a42225e40f
Muhammad Abdul Aziz <64573244+abdul2aziz3@users.noreply.github.com>
2020-05-15 14:57:22 +0800

    Create Jobsheet 1 C.c
    Program array satu dimensi yang menampilkan akhiran NIM mahasiswa secara Statis

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+char nim[5] = {'1', '2', '3', '4', '5'};
+
+printf("Muhammad Abdul Aziz");
+printf("\nNIM : F1B019095");
+printf("\n===================\n\n");
+
+printf ("NIM mahasiswa ke-1 = %c\n", nim[0]);
+printf ("NIM mahasiswa ke-2 = %c\n", nim[1]);
+printf ("NIM mahasiswa ke-3 = %c\n", nim[2]);
+printf ("NIM mahasiswa ke-4 = %c\n", nim[3]);
+printf ("NIM mahasiswa ke-5 = %c\n", nim[4]);
+
+}
