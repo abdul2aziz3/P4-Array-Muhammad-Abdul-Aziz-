571baf8783f3cf3e82a8f85135d7f68253c06e39
Muhammad Abdul Aziz <64573244+abdul2aziz3@users.noreply.github.com>
2020-05-15 13:10:01 +0800

    Create Jobsheet 7 C++.cpp
    Program mengisi nilai array dengan perulangan sebanyak angka NIM terakhir praktikan

---
+++ Jobsheet 7 C++.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int x[5],i;
+  cout << "Muhammad Abdul Aziz\n";
+  cout << "F1B019095\n";
+  cout << "===================\n";
+  for(i=0;i<5;i++){
+  	cout<<"Masukan Nilai "<<i+1<<" : ";
+  	cin>>x[i];
+  }
+  return 0;
+}
