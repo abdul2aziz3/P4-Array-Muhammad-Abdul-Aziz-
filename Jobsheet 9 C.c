1237d51eb9e078342ec0d6ee2d7d5fe0d5a738d1
Muhammad Abdul Aziz <64573244+abdul2aziz3@users.noreply.github.com>
2020-05-15 15:08:18 +0800

    Create Jobsheet 9 C.c
    Program array sebagai karakter dengan menampilkan nama setiap Anggota kelompok 20 yang diinput secara Statis

---
+++ Jobsheet 9 C.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int i;
+	char nama[6]={'A','Z','I','Z'};
+	char nama1[7]={'R','I','E','Z','Q','UL'};
+	char nama2[10]={'N','O','V','R','I','Z','A','L'};
+	char nama3[6]={'I','R','S','A','L'};
+	char nama4[10]={'R','A','R','A','N','T','A'};
+
+	
+	printf("\n");
+	printf("Nama = %s\n\n",nama);
+	printf("Nama = %s\n\n",nama1);
+	printf("Nama = %s\n\n",nama2);
+	printf("Nama = %s\n\n",nama3);
+	printf("Nama = %s\n\n",nama4);
+	return 0;
+}
