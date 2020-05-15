eebc078d772797ecbdfec1ce23654145be5966da
Muhammad Abdul Aziz <64573244+abdul2aziz3@users.noreply.github.com>
2020-05-15 14:59:08 +0800

    Create Jobsheet 2 C.c
    Program mengisi dan menampilkan  NIM terakhir setiap mahasiswa yang di input secara Dinamis

---
+++ Jobsheet 2 C.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+main(){
+int x[5];
+
+printf("Muhammad Abdul Aziz");
+printf("\nNIM : F1B019095");
+printf("\n===================\n\n");
+
+printf("\nMasukkan NIM mahasiswa terakhir ke-1 : ");
+	scanf("%d", &x[0]);
+printf("Masukkan NIM mahasiswa terakhir ke-2 : ");
+	scanf("%d", &x[1]);
+printf("Masukkan NIM mahasiswa terakhir ke-3 : ");
+	scanf("%d", &x[2]);
+printf("Masukkan NIM mahasiswa terakhir ke-4 : ");
+	scanf("%d", &x[3]);
+printf("Masukkan NIM mahasiswa terakhir ke-5 : ");
+	scanf("%d", &x[4]);
+
+
+printf("\nNIM terakhir mahasiswa ke-1 : %d\n", x[0]);
+printf("NIM terakhir mahasiswa ke-2 : %d\n", x[1]);
+printf("NIM terakhir mahasiswa ke-3 : %d\n", x[2]);
+printf("NIM terakhir mahasiswa ke-4 : %d\n", x[3]);
+printf("NIM terakhir mahasiswa ke-5 : %d\n", x[4]);
+
+}
