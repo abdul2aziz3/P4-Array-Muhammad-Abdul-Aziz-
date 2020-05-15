2ba57293dffcd0b16a5ed983538088991d363bb3
Muhammad Abdul Aziz <64573244+abdul2aziz3@users.noreply.github.com>
2020-05-15 13:21:25 +0800

    Create Jobsheet 9 C++.cpp
    Program array bertipe karakter dengan menampilkan nama Anggota kelompok secara Statis

---
+++ Jobsheet 9 C++.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(int argc, char*argv[])
+{
+string nama[5]; 
+int i,j; 
+cout << "Muhammad Abdul Aziz\n";
+cout << "F1B019095\n";
+cout << "===================\n";
+
+nama[0]="Muhammad Riezqul Fikri";
+nama[1]="Muhammad Abdul Aziz";
+nama[2]="Muhammad Novrizal";
+nama[3]="Muhammad Irsal Fajri Efendy";
+nama[4]="Raranta Asmara";
+
+cout<<"nama 1 = "<<nama[0]<<endl;
+cout<<"nama 2 = "<<nama[1]<<endl;
+cout<<"nama 3 = "<<nama[2]<<endl;
+cout<<"nama 4 = "<<nama[3]<<endl;
+cout<<"nama 5 = "<<nama[4]<<endl;
+
+return 0;
+}
